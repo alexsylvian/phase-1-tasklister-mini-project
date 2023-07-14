7a5b5cab73d17dcb414ce3dd24e055ae569402c5
alexsylvian <lazeratus1@gmail.com>
2023-07-13 22:16:54 -0400

    first round works, might add some stuff

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
+const submitter = document.getElementById('create-task-form')
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  submitter.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(e);
+    todoFunction(e.target['new-task-description'].value)
+  })
 });
+
+function todoFunction(todo) {
+  console.log(todo)
+  const p = document.createElement('p')
+  p.innerHTML = `${todo} `
+  const btn = document.createElement('button')
+  btn.innerHTML = 'x'
+  btn.addEventListener('click', (e) => e.target.parentNode.remove())
+  btn.addEventListener('click', (e) => e.target.remove())
+  const tasks = document.getElementById('tasks')
+  if (todo) {
+    tasks.appendChild(p) 
+    p.appendChild(btn)
+  }
+}
